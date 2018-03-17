8011b953b98ea72bbaab427c12fbb66a7dc55d2e
ZhaowWei <1317791477@qq.com>
2018-03-17 15:35:15 +0800

    界面修改
    第一次界面的整合

---
+++ SCU-Community/SCU-Community/app/src/main/java/home/smart/fly/scucommunity/fragments/FourFragment.java
@@ -0,0 +1,65 @@
+package home.smart.fly.scucommunity.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import home.smart.fly.scucommunity.R;
+import home.smart.fly.scucommunity.SecondActivity;
+
+import static home.smart.fly.scucommunity.R.id.headview;
+
+/**
+ * Created by engineer on 2016/9/21.
+ */
+
+public class FourFragment extends Fragment {
+    private View rootView;
+    private Context mContext;
+    private CollapsingToolbarLayout collapsing_toolbar;
+    private FloatingActionButton fab;
+    private static final String picUrl = "http://img1.imgtn.bdimg.com/it/u=3743691986,2983459167&fm=21&gp=0.jpg";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_four, container, false);
+        InitView();
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    private void InitView() {
+        collapsing_toolbar = (CollapsingToolbarLayout) rootView.findViewById(R.id.collapsing_toolbar);
+        collapsing_toolbar.setTitle("个人中心");
+        fab = (FloatingActionButton) rootView.findViewById(R.id.btn);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),SecondActivity.class);
+                startActivity(intent);
+                Toast.makeText(getContext(),"修改个人资料",Toast.LENGTH_SHORT).show();
+            }
+        });
+        CircleImageView view = (CircleImageView) rootView.findViewById(headview);
+        Glide.with(mContext).load(picUrl).placeholder(R.drawable.profile).into(view);
+
+    }
+}
---
+++ SCU-Community/SCU-Community/app/src/main/java/home/smart/fly/scucommunity/fragments/SecondFragment.java
@@ -0,0 +1,64 @@
+package home.smart.fly.scucommunity.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import home.smart.fly.scucommunity.R;
+import home.smart.fly.scucommunity.adapter.FragmentAdapter;
+
+public class SecondFragment extends Fragment {
+    private View rootView;
+    private Context mContext;
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_two, container, false);
+        InitView();
+        return rootView;
+    }
+
+
+    private void InitView() {
+        mTabLayout = (TabLayout) rootView.findViewById(R.id.tabs);
+        mViewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+
+        //初始化TabLayout的title
+        mTabLayout.addTab(mTabLayout.newTab().setText("我的收藏"));
+        List<String> titles = new ArrayList<>();
+        titles.add("我的收藏");
+
+        //初始化ViewPager的数据集
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(new SecondSubFragment());
+        //创建ViewPager的adapter
+        FragmentAdapter adapter = new FragmentAdapter(getChildFragmentManager(), fragments, titles);
+        mViewPager.setAdapter(adapter);
+        //关联TabLayout与ViewPager
+        //同时也要覆写PagerAdapter的getPageTitle方法，否则Tab没有title
+        mTabLayout.setupWithViewPager(mViewPager);
+        mTabLayout.setTabsFromPagerAdapter(adapter);
+
+    }
+
+
+}
