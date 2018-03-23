a9f4e67383fc813f727262f9d1ed1a980d303c7f
ZhaowWei <1317791477@qq.com>
2018-03-23 11:53:29 +0800

    2
    1

--- SCU-Community/app/src/main/java/home/smart/fly/scucommunity/AnswerActivity.java
+++ SCU-Community/app/src/main/java/home/smart/fly/scucommunity/AnswerActivity.java
@@ -1,10 +1,16 @@
 package home.smart.fly.scucommunity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,6 +19,7 @@ import java.util.List;
 import home.smart.fly.scucommunity.adapter.AnswerAdapter;
 import home.smart.fly.scucommunity.content.Answer;
 import home.smart.fly.scucommunity.content.Question;
+import home.smart.fly.scucommunity.content.User;
 import home.smart.fly.scucommunity.util.HttpUtil;
 import home.smart.fly.scucommunity.util.Utility;
 import okhttp3.Call;
@@ -22,17 +29,34 @@ import okhttp3.Response;
 public class AnswerActivity extends AppCompatActivity {
 
     private List<Answer> answerList = new ArrayList<>();
+    private List<Answer> answerListsend = new ArrayList<>();
+    private List<User> userList = new ArrayList<>();
     private Question question = new Question();
+    private Answer answer = new Answer();
+
     private int question_id;
     private String question_name;
     private String question_content;
     private String question_title;
+    private static String user_name;
+    private int user_id;
+    private EditText editText_answer;
+    private ImageButton sent_answer;
+    private String answer_content;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.answer);
+        Log.i("1111","第一个位置");
+
+        SharedPreferences pref = getSharedPreferences("user_id_data",MODE_PRIVATE);
+        user_id = pref.getInt("user_id",0);
+        String uid= Integer.toString(user_id);
+        Log.i("1111",uid);
+        findname(uid);
+        Log.i("2222","第二个位置");
 
         Intent intent = getIntent();
         question_id = intent.getIntExtra("question_id",0);
@@ -53,10 +77,54 @@ public class AnswerActivity extends AppCompatActivity {
         title.setText(question_title);
 
 
+        editText_answer = (EditText)findViewById(R.id.edit_answer) ;
+        answer_content = editText_answer.getText().toString();
+        sent_answer = (ImageButton)findViewById(R.id.imageButton);
+        sent_answer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                answer.setContent(answer_content);
+                answer.setName(user_name);
+                answer.setUid(user_id);
+                answer.setQuestion_ID(question_id);
+                answerListsend.add(answer);
+                HttpUtil.postOkHttpRequestion("http://182.149.199.213:3000/answer/update",answerListsend);
+            }
+        });
+
+
+
+
         initAnswer(qid);
         AnswerAdapter adapter = new AnswerAdapter(AnswerActivity.this, R.layout.answe_item,answerList);
         ListView listView = (ListView) findViewById(R.id.listview_answer);
         listView.setAdapter(adapter);
+    }
+
+    private void findname(String uid){
+        HttpUtil.postOkHttpgetdata("http://182.149.199.213:3000/users/queryById", "uid", uid, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String user = response.body().string();
+                userList = Utility.handleUserResponse(user);
+                User useruu = new User();
+                useruu = userList.get(0);
+                user_name = useruu.getName();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(AnswerActivity.this, "打印"+user_name, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        });
     }
 
     private void initAnswer(String qid){
